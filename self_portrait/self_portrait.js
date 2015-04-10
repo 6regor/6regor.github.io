05f406f21d21e2636d7a0264d61967889b0d5681
6regor <gregorsnare@hotmail.com>
2015-04-10 18:42:05 +1000

    ok

--- self_portrait/self_portrait.js
+++ self_portrait/self_portrait.js
@@ -11,7 +11,7 @@ function preload() {
     this.game.scale.refresh();
     
     game.load.image('infosheet', 'infosheet.png')
-    game.load.spritesheet('gregor', 'gregor.png', 198, 439, 11);
+    game.load.spritesheet('gregor', 'gregor.png', 198, 439, 17);
     
     game.load.audio('music', [ 'music.mp3', 'music.ogg' ]);
 }
@@ -33,12 +33,21 @@ function create() {
     Xkey = game.input.keyboard.addKey(Phaser.Keyboard.X);
     
      gregor.animations.add('anti', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7, false, true);
+     gregor.animations.add('fetaldown', [10, 11, 12, 13, 14, 15, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16], 7, false, true);
+     gregor.animations.add('fetalup', [16, 15, 14, 13, 12, 11, 10], 7, false, true);
      gregor.animations.add('clock', [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 7, false, true);
+     gregor.animations.add('stay', [10], 7, false, true);
+     
     
     
 }
 
 function update(){ 
+    
+    var wasdown;
+    wasdown = 0;
+    
+    //cursors.down.onUp.add(playfetalup);
     
     if (ikey.isDown){
         var infosheet = game.add.sprite(0, 0, 'infosheet');
@@ -64,7 +73,30 @@ function update(){
         
     }
     
+    else if (cursors.down.isDown){
+        
+        gregor.animations.play('fetaldown');
+        wasdown = 0;
+        
+    }
+    
+    //else if (cursors.up.isDown){
+       // if (wasdown = 1){
+        //    gregor.animations.play('fetalup');
+        //    wasdown = 2;
+      //  }
+      //  else{
+       //     gregor.animations.play('stay');
+      //  }
+  //  }
+    
+    
     else{
         gregor.animations.stop();
     }
+    
 }
+
+//function playfetalup() {
+  //  gregor.animations.play('fetalup');
+    //gregor.animations.stop();
